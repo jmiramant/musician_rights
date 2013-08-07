db4c50dbbf031f142cc80c2ee31467a3cf61b46b
jmiramant <josh@miramant.me>
2013-08-07 16:54:36 -0700

    updated mailchimp

--- app/models/mail_chimp.rb
+++ app/models/mail_chimp.rb
@@ -2,8 +2,8 @@ class MailChimp
   include Hominid
 
   def self.add_user(user)
-    h = Hominid::API.new(MAIL_CHIMP[Rails.env]['api_key'])
-    h.list_subscribe(MAIL_CHIMP[Rails.env]['subscriber_list'], user.email, {'FNAME' => user.first_name, 'LNAME' => user.last_name}, 'html', false, true, true, false)
+    h = Hominid::API.new('91f455bf0d3448173043fd334cbec463-us7')
+    h.list_subscribe('61d4987e01', user.email, {'FNAME' => user.first_name, 'LNAME' => user.last_name}, 'html', false, true, true, false)
   end
 
   def self.unsubscribe_user(user)
