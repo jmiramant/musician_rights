7f667cf9b1cdf31a97646a06b192f773328260c6
jmiramant <josh@miramant.me>
2013-08-15 19:14:39 -0700

    temp fix to zip

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 
   validates_presence_of :first_name, :last_name, :email, :zipcode
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
-  validates_format_of :zipcode, :with => /^\d{5}(-\d{4})?$/, :message => "should be in the form 12345"
+  # validates_format_of :zipcode, :with => /\w{5}/, :message => "should be in the form 12345"
 
 	def create_user_record_in_mailchimp
 		begin
