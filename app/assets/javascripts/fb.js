944a0b88c3bbfa84fbfaad655122d33d82471695
jmiramant <josh@miramant.me>
2013-08-18 12:46:57 -0700

    fixed facebook

--- app/assets/javascripts/fb.js
+++ app/assets/javascripts/fb.js
@@ -1,20 +1,20 @@
-<div id="fb-root"></div>
-  window.fbAsyncInit = function() {
-    // init the FB JS SDK
-    FB.init({
-      appId      : '547993098582705',                        // App ID from the app dashboard
-      channelUrl : '//www.musicianrights.com/channel.html', // Channel file for x-domain comms
-      status     : true,                                 // Check Facebook Login status
-      xfbml      : true                                  // Look for social plugins on the page
-    });
+window.fbAsyncInit = function() {
+  FB.init({
+    appId: '547993098582705',
+    channelUrl: '//www.musicianrights.com/channel.html',
+    status: true,
+    xfbml: true
+  });
 
-    // Additional initialization code such as adding Event Listeners goes here
-  };
+};
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/all.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
